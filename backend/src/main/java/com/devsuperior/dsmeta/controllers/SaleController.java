a7bac68f989446e113f96a5e615b96278869a9e9
Jeferson Xavier <98062610+JefersonXavierr@users.noreply.github.com>
2022-09-08 19:43:56 -0300

    APITest

---
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dsmeta.controllers;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.services.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+    private final SaleService service;
+
+    public SaleController(SaleService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Sale> findSales() {
+        return service.findSales();
+    }
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmeta/controllers/SaleControllerTest.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dsmeta.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaleControllerTest {
+
+    @Test
+    void findSales() {
+    }
+}
